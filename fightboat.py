2da3c5b8e9015068efda5aa681e38462e4937857
Justin Phelps <linuturk@onitato.com>
2011-03-10 22:19:50 -0600

    Added some comments to the code, and documented my function.

--- fightboat.py
+++ fightboat.py
@@ -5,9 +5,10 @@ import random
 print 'Welcome to Fight Boat!'
 print 'Prepare to die!'
 
-grid = ( 10, 10 )
+grid = ( 10, 10 ) # Max (x,y) value for the grid
 
 def random_ships(grid):
+        '''Takes a tuple (x,y) where x and y are the boundry for the playing field.'''
         ships = {}
         while len(ships) <= 4:
                 cord_x = random.choice(range(1, grid[0] + 1))
