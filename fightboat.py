b9d435684dd07048872ca8af03fca061561f86f7
Justin Phelps <linuturk@onitato.com>
2011-03-14 15:46:08 -0500

    Made some changes to the code recommended by sontek in #pensacola_linux on irc.freenode.net

--- fightboat.py
+++ fightboat.py
@@ -2,9 +2,14 @@
 
 import random
 
-debug = 1 # Debug information switch
-grid = ( 10, 10 ) # Max (x,y) value for the grid
-max_ships = 5 # Number of ships for each player
+# Debug information switch
+DEBUG = 1
+
+# Grid Dimensions
+MAX_X = 10
+MAX_Y = 10
+
+MAX_SHIPS = 5 # Number of ships for each player
 
 def introduction():
         '''Displays an introduction to the game.'''
@@ -12,12 +17,12 @@ def introduction():
         print 'Prepare to die!'
         return None
 
-def random_ships(grid, max_ships):
+def random_ships():
         '''Takes a tuple (x,y) where x and y are the boundry for the playing field.'''
         ships = {}
-        while len(ships) <= max_ships - 1:
-                cord_x = random.choice(range(1, grid[0] + 1))
-                cord_y = random.choice(range(1, grid[1] + 1))
+        while len(ships) <= MAX_SHIPS - 1:
+                cord_x = random.choice(range(1, MAX_X + 1))
+                cord_y = random.choice(range(1, MAX_Y + 1))
                 ships[cord_x] = cord_y
         return ships
 
@@ -26,26 +31,32 @@ def are_all_ships_dead(player):
 	if len(player) == 0: return True
 	else: return False
 
+def get_valid_input(question):
+        '''Takes a string as the question being asked and makes sure the value returned is an int.'''
+        try:
+                guess = int(raw_input('%s:\n' % question))
+                return guess
+        except ValueError:
+                print 'Sorry input was invalid, please provide a number'
+                return get_valid_input(question)
+
 def take_a_shot():
         '''The player takes a shot. This function takes user input and returns values to append to a dictionary of the user's shots.'''
-        print 'Enter your x guess:'
-        x_guess = raw_input()
-        print 'Enter your y guess:'
-        y_guess = raw_input()
+        x_guess = get_valid_input('Enter your x guess')
+        y_guess = get_valid_input('Enter your y guess')
 
         x_guess = int(x_guess)
         y_guess = int(y_guess)
         guess = {x_guess: y_guess}
 
         print 'You guessed', guess
-        print 'Is this correct? (y or n)'
-        yes_or_no = raw_input()
+        yes_or_no = raw_input('Is this correct? (y or n) ').lower()
 
         if yes_or_no == 'y': return guess
-        elif yes_or_no == 'n': take_a_shot()
+        elif yes_or_no == 'n': return take_a_shot()
         else:
                 print 'Invalid Entry. Try again.'
-                take_a_shot()
+                return take_a_shot()
 
 def coin_flip():
         '''Returns heads (True) or tails (False)'''
@@ -57,13 +68,13 @@ def coin_flip():
 introduction()
 
 # Setup random ship locations for both players.
-player1_ships = random_ships(grid, max_ships)
-player2_ships = random_ships(grid, max_ships)
+player1_ships = random_ships()
+player2_ships = random_ships()
 
 # Flip a coin to see who goes first.
 
 # Display debugging information
-if debug == 1:
+if DEBUG == 1:
         print 'Player 1 Ship Locations:', player1_ships
         print 'Player 2 Ship Locations:', player2_ships
         print 'Is player 1 dead?:', are_all_ships_dead(player1_ships)
